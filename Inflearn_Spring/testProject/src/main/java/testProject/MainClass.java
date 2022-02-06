71db9efda2717d2f082d8a62e358452bc3a36f84
강원기/Cine <kwg940930@google.com>
2022-02-06 18:27:16 +0900

    인프런 Spring

---
+++ Inflearn_Spring/testProject/src/main/java/testProject/MainClass.java
@@ -0,0 +1,10 @@
+package testProject;
+
+public class MainClass {
+	
+	public static void main(String[] args) {
+		TransportationWalk transportation = new TransportationWalk();
+		transportation.move();
+	}
+
+}
